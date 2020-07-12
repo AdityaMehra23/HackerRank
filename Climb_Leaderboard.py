78bf12e381019559d3809af117da9b18cd263aad
aditya mehra <mehra.aditya1503@gmail.com>
2020-07-12 12:35:37 +0530

    Climbing Leaderboard

---
+++ Climb_Leaderboard.py
@@ -0,0 +1,15 @@
+
+n = int(input().strip())
+
+scores = list(reversed(sorted(list(set([int(scores_temp) for scores_temp in input().strip().split(' ')])))))
+
+
+m = int(input().strip())
+
+alice = [int(alice_temp) for alice_temp in input().strip().split(' ')]
+
+for x in alice:
+    while len(scores) > 0 and scores[-1] <= x:
+        scores.pop(-1)
+    
+    print(len(scores) + 1)
